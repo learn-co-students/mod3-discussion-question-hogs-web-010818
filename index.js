9b2057d031be0d8991d365d454ad3485cef79622
feihafferkamp <fei.hafferkamp@gmail.com>
2018-02-27 10:05:04 -0500

    Done.

--- index.js
+++ index.js
@@ -25,26 +25,31 @@ function transitionPage(el, groupOut, groupIn) {
 }
 /*************************************************************************/
 
-function delayedFadeOut(div, range) {
-  // Your solution here
-  fadeOut(div)
+function delayedFadeOut(div, range, min = 0) {
+  setTimeout(() => {
+      fadeOut(div)
+    }, Math.random() * range + min)
+  // setTimeout(fadeOut, Math.random() * range + min, div);
 }
 
-function delayedFadeIn(div, range) {
-  // Your solution here
-  fadeIn(div)
+function delayedFadeIn(div, range, min = 0) {
+  // setTimeout(fadeIn, Math.random() * range + min, div);
+  setTimeout(() => {
+    fadeIn(div)
+  }, Math.random() * range + min)
 }
 
 function fadeAllOut(el, group) {
-  // Your solution here
   group.forEach(div => {
-    delayedFadeOut(div)
-  })
+    if (div !== el) {
+      delayedFadeOut(div, SHORTRANGE)
+    }
+    delayedFadeOut(el, LONGRANGE, MINDELAY);
+  });
 }
 
 function fadeAllIn(group) {
-  // Your solution here
   group.forEach(div => {
-    delayedFadeIn(div)
+    delayedFadeIn(div, SHORTRANGE)
   })
 }
